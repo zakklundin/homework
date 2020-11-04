892d3ba2eaef738ff8a6aac2f22ddc27289270ed
Zakk Lundin <zakk.lundin@skola.botkyrka.se>
2020-11-04 08:58:33 +0100

    Solved 2

---
+++ 45/2/script.js
@@ -0,0 +1,7 @@
+function textLength(str){
+    return str.length
+}
+
+console.log(textLength("foo"))      // Prints 3
+console.log(textLength(""))         // Prints 0
+console.log(textLength("Tullinge")) // Prints 8
