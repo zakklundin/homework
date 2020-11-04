5c8a264421c3e636265423dfc4e249aae7a6e2d9
Zakk Lundin <zakk.lundin@skola.botkyrka.se>
2020-11-04 09:04:19 +0100

    Solved 3

---
+++ 45/3/script.js
@@ -0,0 +1,7 @@
+function wordCount(str) {
+    return str.split(" ").length
+}
+
+console.log(wordCount("foo"))               // Prints 1
+console.log(wordCount("Jag älskar chips"))  // Prints 3
+console.log(wordCount("God natt!"))         // Prints 2
