fa2468bdcb9bd60fd734aa957d3221d61601b91e
Zakk Lundin <zakk.lundin@skola.botkyrka.se>
2020-11-10 17:32:02 +0100

    Uppgift 3

---
+++ 46/3/script.js
@@ -0,0 +1,12 @@
+let animal = {
+    species: "Cat",
+    name: "Catman",
+    age: 11
+}
+console.log(animal)
+
+animal.age++
+animal.age = animal.age + 1
+//båda fungerar individuellt, tillsammans blir de +2
+
+console.log(animal)
