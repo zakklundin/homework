251f7e93a53015c4b66974f4c9f2ca8a5c697419
Einar Persson <me@einarpersson.com>
2020-11-23 03:20:18 +0100

    48

---
+++ 48/1/script.js
@@ -0,0 +1,3 @@
+// Deklarera en variabel name och ge den värdet av ditt namn
+
+// Skriv en for-loop som loggar ut "Hejsan <ditt namn här>!!" 500 gånger
---
+++ 48/2/script.js
@@ -0,0 +1,14 @@
+// Skriv en while loop
+// Som varje iteration (repetition) slumpar ett heltal mellan 0 och 10
+// Om talet blir 6, avsluta loopen
+
+
+// Exempel på output:
+// 4
+// 7
+// 3
+// 8
+// 4
+// 1
+// 1
+// 6
